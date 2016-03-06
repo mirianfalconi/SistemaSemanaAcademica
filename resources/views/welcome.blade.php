c29b09beeab3f2820ea554604dec09275f3d59cd
mirianfalconi <mirianfalconi@yahoo.com.br>
2016-03-06 14:05:46 -0300

    new file:   resources/views/task.blade.php 	modified:   resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,18 @@
-<html>
-	<head>
-		<title>Laravel</title>
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-		<link href="{{ asset('css/grid.css') }}" rel="stylesheet">
-	</head>
-	<body>
-		@extends('layouts.app')
-	  @section('content')
-	  @if (count($tasks) > 0)
-	    <article>
-	      <table>
-	        <div class="row">
-	            <div class="column column-4"> Quinta-feira </div>
-	            <div class="column column-4"> Sexta-feira </div>
-	            <div class="column column-4"> Sabado </div>
-	            @foreach ($tasks as $task)
-	              <div class="column column-4"> {{ $task->name }} </div>
-	            @endforeach
-	        </div>
-	    </article>
-	    @endif
-	  @endsection
-	</body>
-</html>
+<!DOCTYPE html>
+		<html lang="en">
+		    <head>
+		        <title>Laravel Quickstart - Basic</title>
+
+		        <!-- <link href="{{ asset('css/grid.css') }}" rel="stylesheet"> -->
+		    </head>
+
+		    <body>
+		        <div class="container">
+		            <nav class="navbar navbar-default">
+		                <!-- Navbar Contents -->
+		            </nav>
+		        </div>
+
+		        @yield('content')
+		    </body>
+		</html>
