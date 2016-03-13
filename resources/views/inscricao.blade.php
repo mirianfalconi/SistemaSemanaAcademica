bf426101398ff2a5ab008bec5b56778c938a1690
mirianfalconi <mirianfalconi@yahoo.com.br>
2016-03-13 14:08:18 -0300

    new file:   public/css/form.css 	new file:   public/js/jquery.maskedinput.min.js 	modified:   resources/views/inscricao.blade.php 	modified:   resources/views/layout/app.blade.php 	new file:   resources/views/layout/form.blade.php

--- resources/views/inscricao.blade.php
+++ resources/views/inscricao.blade.php
@@ -1,20 +1,20 @@
-@extends('layout.app')
+@extends('layout.form')
 
 @section('content')
 
-        {!! Form::open(['url'=>'cadastro','id'=>'sign-up']) !!}
+        {!! Form::open(['url'=>'cadastro','id'=>'sign-up', 'class'=>'smart']) !!}
 
-        {!! Form::label('cpf', 'CPF:') !!}
-        {!! Form::text('cpf', null)!!}
+        <h1>Cadastro
+            <span>Preencha todos os campos.</span>
+        </h1>
 
-        {!! Form::label('nome', 'Nome:') !!}
-        {!! Form::text('nome', null)!!}
+        {!! Form::text('cpf', null, ['placeholder' => 'CPF', 'pattern' => '\d{3}\.\d{3}\.\d{3}-\d{2}'])!!}
 
-        {!! Form::label('telefone', 'Telefone:') !!}
-        {!! Form::text('telefone', null)!!}
+        {!! Form::text('nome', null, ['placeholder' => 'Nome', 'pattern' => '[a-zA-Z\s]+$', 'max' =>'45'])!!}
 
-        {!! Form::label('email', 'Email:') !!}
-        {!! Form::text('email', null)!!}
+        {!! Form::text('telefone', null, ['placeholder' => 'Telefone', 'pattern' => '\([0-9]{2}\) [0-9]{4,6}-[0-9]{3,4}$'])!!}
+
+        {!! Form::text('email', null, ['placeholder' => 'Email', 'max' =>'45'])!!}
 
         @unless($errors->isEmpty())
             <ul>
@@ -24,7 +24,9 @@
             </ul>
         @endunless
 
-        {!! Form::submit('Enviar') !!}
+        <br><br>
+
+        {!! Form::submit('Enviar', ['class'=>'btn']) !!}
         {!! Form::close() !!}
 
         <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
